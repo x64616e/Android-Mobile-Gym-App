b10eee001b9bf01e3f8be9965c46dc4b8108a6b4
GreyFox48 <mattcolsen@att.net>
2019-10-15 11:39:19 -0700

    Added Plan Routine
    Only Leg Day works for prototype

--- JImv2/app/src/main/java/com/example/jimv2/PlanWorkoutActivity.java
+++ JImv2/app/src/main/java/com/example/jimv2/PlanWorkoutActivity.java
@@ -1,40 +1,42 @@
 package com.example.jimv2;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.view.View;
 import android.widget.Button;
 
-public class PlanRoutineActivity extends AppCompatActivity {
-
+public class PlanWorkoutActivity extends AppCompatActivity {
 
     private Button doneButton;
-    private Button planExercise;
+    private Button addExercise;
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.plan_routine);
+        setContentView(R.layout.plan_workout);
 
-        doneButton = (Button) findViewById(R.id.planRoutineDone);
-        planExercise = (Button) findViewById(R.id.legDay);
+        doneButton = (Button) findViewById(R.id.planWorkoutDone);
+        addExercise = (Button) findViewById(R.id.addExerciseToWorkout);
         doneButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
             }
         });
-        planExercise.setOnClickListener(new View.OnClickListener() {
+        addExercise.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                toPlanWorkoutActivity();
+                toAddExercise();
             }
         });
     }
 
-    public void toPlanWorkoutActivity(){
-        Intent intent = new Intent(this, PlanWorkoutActivity.class);
+    public void toAddExercise(){
+        Intent intent = new Intent(this, AddExcercise.class);
         startActivity(intent);
     }
 }
+
+
