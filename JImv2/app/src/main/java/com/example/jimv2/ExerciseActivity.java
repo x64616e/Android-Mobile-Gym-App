0a6866ae4bf0fe1b44a8516525df8d4fa3fa8d32
x64616e <45182896+x64616e@users.noreply.github.com>
2019-10-23 10:48:51 -0700

    Timer Implementation

--- JImv2/app/src/main/java/com/example/jimv2/ExerciseActivity.java
+++ JImv2/app/src/main/java/com/example/jimv2/ExerciseActivity.java
@@ -4,16 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import java.util.Locale;
 
 public class ExerciseActivity extends AppCompatActivity {
 
 
     private Button doneButton;
     private Button calculatorButton;
+
+    private static final long START_TIME_IN_MILLS = 30000;
+
+    private TextView mTextViewCountDown;
+    private Button mButtonStartPause;
+    private Button mButtonReset;
+
+    private CountDownTimer mCountDownTimer;
+
+    private boolean mTimerRunning;
+
+    private long mTimeLeftInMillis = START_TIME_IN_MILLS;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +41,28 @@ public class ExerciseActivity extends AppCompatActivity {
         imageview.setImageResource(imageRes);
         TextView exerciseName = findViewById(R.id.exercise_name_exerciseAct);
         exerciseName.setText(name);
+
+        mTextViewCountDown = findViewById(R.id.text_view_countdown);
+        mButtonStartPause = findViewById(R.id.button_start_pause);
+        mButtonReset = findViewById(R.id.button_reset);
+
+        mButtonStartPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if ( mTimerRunning){
+                    pauseTimer();
+                }else
+                    startTimer();
+            }
+        });
+        mButtonReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resetTimer();
+            }
+        });
+
+        updateCountDownText();
 
         doneButton = (Button) findViewById(R.id.exerciseDoneButton);
         doneButton.setOnClickListener(new View.OnClickListener() {
@@ -45,8 +81,54 @@ public class ExerciseActivity extends AppCompatActivity {
         });
 
     }
+
+    private void startTimer() {
+        mCountDownTimer = new CountDownTimer(mTimeLeftInMillis, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                mTimeLeftInMillis = millisUntilFinished;
+                updateCountDownText();
+            }
+
+            @Override
+            public void onFinish() {
+                mTimerRunning = false;
+                mButtonStartPause.setText("Start");
+                mButtonStartPause.setVisibility(View.INVISIBLE);
+                mButtonReset.setVisibility(View.VISIBLE);
+
+            }
+        }.start();
+
+        mTimerRunning = true;
+        mButtonStartPause.setText("pause");
+        mButtonReset.setVisibility(View.INVISIBLE);
+    }
+
+    private void pauseTimer() {
+        mCountDownTimer.cancel();
+        mTimerRunning = false;
+        mButtonStartPause.setText("Start");
+        mButtonReset.setVisibility(View.VISIBLE);
+    }
+    private void resetTimer() {
+        mTimeLeftInMillis = START_TIME_IN_MILLS;
+        updateCountDownText();
+        mButtonReset.setVisibility(View.INVISIBLE);
+        mButtonStartPause.setVisibility(View.VISIBLE);
+    }
+
+    private void updateCountDownText() {
+        int minutes = (int) (mTimeLeftInMillis/1000) / 60;
+        int seconds = (int) (mTimeLeftInMillis/1000) % 60;
+
+        String timeLeftFormatted = String.format(Locale.getDefault(),"%02d:%02d", minutes,seconds);
+
+        mTextViewCountDown.setText(timeLeftFormatted);
+    }
+
     public void backToWorkout(){
-    finish();
+        finish();
     }
     public void openCalculatorScreen(){
         Intent intent = new Intent(this,CalculatorActivity.class);
