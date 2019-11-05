ac94e5890d9ab89034704777ca56a32317cbb012
x64616e <45182896+x64616e@users.noreply.github.com>
2019-11-04 18:59:48 -0800

    Heart Rate Monitor Changes

--- JImv2/app/src/main/java/com/example/jimv2/HeartbeatView.java
+++ JImv2/app/src/main/java/com/example/jimv2/HeartbeatView.java
@@ -24,15 +24,15 @@ public class HeartbeatView extends View {
     public HeartbeatView(Context context, AttributeSet attr) {
         super(context, attr);
 
-        greenBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.green_icon);
-        redBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.red_icon);
+        greenBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.red);
+        redBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.blue);
     }
 
     public HeartbeatView(Context context) {
         super(context);
 
-        greenBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.green_icon);
-        redBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.red_icon);
+        greenBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.red);
+        redBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.blue);
     }
 
     /**
