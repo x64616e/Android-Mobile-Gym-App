2db3e8b747b63310db636b808fc06640321d4ece
Jason Nyland <jason@jasonnyland.com>
2019-12-04 12:41:46 -0800

    increments but never stops

--- JImv2/app/src/main/java/com/example/jimv2/ExerciseActivity.java
+++ JImv2/app/src/main/java/com/example/jimv2/ExerciseActivity.java
@@ -1,5 +1,6 @@
 package com.example.jimv2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import java.lang.*;
 
@@ -8,6 +9,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.renderscript.Sampler;
 import android.text.TextUtils;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
@@ -19,8 +21,11 @@ import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -296,6 +301,31 @@ public class ExerciseActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    private void incrementScore() {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String userID = user.getUid();
+        DatabaseReference userDB = FirebaseDatabase.getInstance().getReference(userID);
+        ValueEventListener userListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                ProfileObject profileObject = dataSnapshot.getValue(ProfileObject.class);
+                int score = profileObject.getScore() + 1;
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                String userID = user.getUid();
+                DatabaseReference userDB = FirebaseDatabase.getInstance().getReference(userID);
+                profileObject.setScore(score);
+                userDB.setValue(profileObject);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        };
+        userDB.addValueEventListener(userListener);
+        Toast.makeText(this, "Good Job!  Score Increased", Toast.LENGTH_LONG).show();
+    }
+
     private void addExercise() {
         try {
             String name = exerciseName.getText().toString().trim();
@@ -310,7 +340,8 @@ public class ExerciseActivity extends AppCompatActivity {
                 String id = databaseExercise.push().getKey();
                 ExerciseObject exercise = new ExerciseObject(1, name, 1,exerciseSets , exerciseReps, exerciseWeight, "");
                 databaseExercise.child(id).setValue(exercise);
-                Toast.makeText(this, "Statistics Recorded", Toast.LENGTH_LONG).show();
+                incrementScore();
+                //Toast.makeText(this, "Statistics Recorded", Toast.LENGTH_LONG).show();
             } else {
                 Toast.makeText(this, "Please Enter the Info", Toast.LENGTH_LONG).show();
                 finish();
